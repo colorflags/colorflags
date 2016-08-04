1675c7180d31178f9aec6330733b3a09d64cfccc
Sam Englander <sizeoverload@gmail.com>
2016-08-04 18:52:31 -0400

    omg

---
+++ lua-sheets/buttons.lua
@@ -0,0 +1,531 @@
+--
+-- created with TexturePacker (http://www.codeandweb.com/texturepacker)
+--
+-- $TexturePacker:SmartUpdate:9c9d0b74d1e24de2096fd6ea34cb9434:1401cc83ff3a9b19da45a14490727340:5f466bc56cff131e087ca92862900388$
+--
+-- local sheetInfo = require("mysheet")
+-- local myImageSheet = graphics.newImageSheet( "mysheet.png", sheetInfo:getSheet() )
+-- local sprite = display.newSprite( myImageSheet , {frames={sheetInfo:getFrameIndex("sprite")}} )
+--
+
+local SheetInfo = {}
+
+SheetInfo.sheet =
+{
+    frames = {
+    
+        {
+            -- About1
+            x=1050,
+            y=0,
+            width=166,
+            height=47,
+
+        },
+        {
+            -- About2
+            x=1216,
+            y=0,
+            width=166,
+            height=47,
+
+        },
+        {
+            -- About3
+            x=1382,
+            y=0,
+            width=166,
+            height=47,
+
+        },
+        {
+            -- About4
+            x=1548,
+            y=0,
+            width=166,
+            height=47,
+
+        },
+        {
+            -- About5
+            x=1714,
+            y=0,
+            width=166,
+            height=47,
+
+        },
+        {
+            -- Again1
+            x=682,
+            y=140,
+            width=156,
+            height=54,
+
+        },
+        {
+            -- Again2
+            x=838,
+            y=141,
+            width=156,
+            height=54,
+
+        },
+        {
+            -- Again3
+            x=994,
+            y=141,
+            width=156,
+            height=54,
+
+        },
+        {
+            -- Again4
+            x=1150,
+            y=141,
+            width=156,
+            height=54,
+
+        },
+        {
+            -- Again5
+            x=1306,
+            y=141,
+            width=156,
+            height=54,
+
+        },
+        {
+            -- Back1
+            x=1880,
+            y=0,
+            width=137,
+            height=47,
+
+        },
+        {
+            -- Back2
+            x=0,
+            y=46,
+            width=137,
+            height=47,
+
+        },
+        {
+            -- Back3
+            x=137,
+            y=46,
+            width=137,
+            height=47,
+
+        },
+        {
+            -- Back4
+            x=274,
+            y=46,
+            width=137,
+            height=47,
+
+        },
+        {
+            -- Back5
+            x=411,
+            y=46,
+            width=137,
+            height=47,
+
+        },
+        {
+            -- Cruise1
+            x=548,
+            y=46,
+            width=178,
+            height=47,
+
+        },
+        {
+            -- Cruise2
+            x=726,
+            y=46,
+            width=178,
+            height=47,
+
+        },
+        {
+            -- Cruise3
+            x=1050,
+            y=47,
+            width=178,
+            height=47,
+
+        },
+        {
+            -- Cruise4
+            x=1228,
+            y=47,
+            width=178,
+            height=47,
+
+        },
+        {
+            -- Cruise5
+            x=1406,
+            y=47,
+            width=178,
+            height=47,
+
+        },
+        {
+            -- Options1
+            x=1462,
+            y=141,
+            width=215,
+            height=56,
+
+        },
+        {
+            -- Options2
+            x=564,
+            y=194,
+            width=215,
+            height=56,
+
+        },
+        {
+            -- Options3
+            x=1343,
+            y=197,
+            width=215,
+            height=56,
+
+        },
+        {
+            -- Options4
+            x=1558,
+            y=202,
+            width=215,
+            height=56,
+
+        },
+        {
+            -- Options5
+            x=1773,
+            y=202,
+            width=215,
+            height=56,
+
+        },
+        {
+            -- PlayGame1
+            x=1677,
+            y=147,
+            width=282,
+            height=55,
+
+        },
+        {
+            -- PlayGame2
+            x=0,
+            y=187,
+            width=282,
+            height=55,
+
+        },
+        {
+            -- PlayGame3
+            x=282,
+            y=190,
+            width=282,
+            height=55,
+
+        },
+        {
+            -- PlayGame4
+            x=779,
+            y=195,
+            width=282,
+            height=55,
+
+        },
+        {
+            -- PlayGame5
+            x=1061,
+            y=195,
+            width=282,
+            height=55,
+
+        },
+        {
+            -- Quit1
+            x=1908,
+            y=47,
+            width=123,
+            height=50,
+
+        },
+        {
+            -- Quit2
+            x=1724,
+            y=94,
+            width=123,
+            height=50,
+
+        },
+        {
+            -- Quit3
+            x=1847,
+            y=97,
+            width=123,
+            height=50,
+
+        },
+        {
+            -- Quit4
+            x=436,
+            y=140,
+            width=123,
+            height=50,
+
+        },
+        {
+            -- Quit5
+            x=559,
+            y=140,
+            width=123,
+            height=50,
+
+        },
+        {
+            -- Restart1
+            x=0,
+            y=0,
+            width=210,
+            height=46,
+
+        },
+        {
+            -- Restart2
+            x=210,
+            y=0,
+            width=210,
+            height=46,
+
+        },
+        {
+            -- Restart3
+            x=420,
+            y=0,
+            width=210,
+            height=46,
+
+        },
+        {
+            -- Restart4
+            x=630,
+            y=0,
+            width=210,
+            height=46,
+
+        },
+        {
+            -- Restart5
+            x=840,
+            y=0,
+            width=210,
+            height=46,
+
+        },
+        {
+            -- Share1
+            x=1584,
+            y=47,
+            width=162,
+            height=47,
+
+        },
+        {
+            -- Share2
+            x=1746,
+            y=47,
+            width=162,
+            height=47,
+
+        },
+        {
+            -- Share3
+            x=0,
+            y=93,
+            width=162,
+            height=47,
+
+        },
+        {
+            -- Share4
+            x=162,
+            y=93,
+            width=162,
+            height=47,
+
+        },
+        {
+            -- Share5
+            x=324,
+            y=93,
+            width=162,
+            height=47,
+
+        },
+        {
+            -- Start1
+            x=904,
+            y=46,
+            width=146,
+            height=47,
+
+        },
+        {
+            -- Start2
+            x=486,
+            y=93,
+            width=146,
+            height=47,
+
+        },
+        {
+            -- Start3
+            x=632,
+            y=93,
+            width=146,
+            height=47,
+
+        },
+        {
+            -- Start4
+            x=778,
+            y=93,
+            width=146,
+            height=47,
+
+        },
+        {
+            -- Start5
+            x=924,
+            y=94,
+            width=146,
+            height=47,
+
+        },
+        {
+            -- Tutorial1
+            x=1070,
+            y=94,
+            width=218,
+            height=47,
+
+        },
+        {
+            -- Tutorial2
+            x=1288,
+            y=94,
+            width=218,
+            height=47,
+
+        },
+        {
+            -- Tutorial3
+            x=1506,
+            y=94,
+            width=218,
+            height=47,
+
+        },
+        {
+            -- Tutorial4
+            x=0,
+            y=140,
+            width=218,
+            height=47,
+
+        },
+        {
+            -- Tutorial5
+            x=218,
+            y=140,
+            width=218,
+            height=47,
+
+        },
+    },
+    
+    sheetContentWidth = 2031,
+    sheetContentHeight = 258
+}
+
+SheetInfo.frameIndex =
+{
+
+    ["About1"] = 1,
+    ["About2"] = 2,
+    ["About3"] = 3,
+    ["About4"] = 4,
+    ["About5"] = 5,
+    ["Again1"] = 6,
+    ["Again2"] = 7,
+    ["Again3"] = 8,
+    ["Again4"] = 9,
+    ["Again5"] = 10,
+    ["Back1"] = 11,
+    ["Back2"] = 12,
+    ["Back3"] = 13,
+    ["Back4"] = 14,
+    ["Back5"] = 15,
+    ["Cruise1"] = 16,
+    ["Cruise2"] = 17,
+    ["Cruise3"] = 18,
+    ["Cruise4"] = 19,
+    ["Cruise5"] = 20,
+    ["Options1"] = 21,
+    ["Options2"] = 22,
+    ["Options3"] = 23,
+    ["Options4"] = 24,
+    ["Options5"] = 25,
+    ["PlayGame1"] = 26,
+    ["PlayGame2"] = 27,
+    ["PlayGame3"] = 28,
+    ["PlayGame4"] = 29,
+    ["PlayGame5"] = 30,
+    ["Quit1"] = 31,
+    ["Quit2"] = 32,
+    ["Quit3"] = 33,
+    ["Quit4"] = 34,
+    ["Quit5"] = 35,
+    ["Restart1"] = 36,
+    ["Restart2"] = 37,
+    ["Restart3"] = 38,
+    ["Restart4"] = 39,
+    ["Restart5"] = 40,
+    ["Share1"] = 41,
+    ["Share2"] = 42,
+    ["Share3"] = 43,
+    ["Share4"] = 44,
+    ["Share5"] = 45,
+    ["Start1"] = 46,
+    ["Start2"] = 47,
+    ["Start3"] = 48,
+    ["Start4"] = 49,
+    ["Start5"] = 50,
+    ["Tutorial1"] = 51,
+    ["Tutorial2"] = 52,
+    ["Tutorial3"] = 53,
+    ["Tutorial4"] = 54,
+    ["Tutorial5"] = 55,
+}
+
+function SheetInfo:getSheet()
+    return self.sheet;
+end
+
+function SheetInfo:getFrameIndex(name)
+    return self.frameIndex[name];
+end
+
+return SheetInfo
